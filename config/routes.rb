9e76837090c5d6788037869fa0773cd11b438e1b
Alex Thompson <alexthompson288@gmail.com>
2014-02-16 22:59:51 +0000

    moved home1 to root and added apps to the header

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Pipwebsite::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'pages#home'
+   root 'pages#home1'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
