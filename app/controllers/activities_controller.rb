4e943c4629ca81009da948d1c46ffa43470c1417
Alex Thompson <alexthompson@Alexs-MacBook-Pro.local>
2014-03-27 16:56:08 +0000

    updated mac download info and ordering activites

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,7 +4,7 @@ class ActivitiesController < ApplicationController
   # GET /activities
   # GET /activities.json
   def index
-    @activities = Activity.all
+    @activities = Activity.order("created_at DESC")
   end
 
   # GET /activities/1
